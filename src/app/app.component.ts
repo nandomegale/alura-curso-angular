07e0d65214fb81dd8268e46eee42553fb26deeb5
Luiz Fernando Megale Paiva <luiz_paiva@gec.inatel.br>
2022-10-06 22:50:03 -0300

    Iniciando Curso Angular da alura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alurapic';
+
+  photos = [];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
